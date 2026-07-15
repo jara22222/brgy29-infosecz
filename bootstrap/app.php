f322bb74aa7372a6c9a8e7b65a1adc48d26181de
jara22222 <jarajoaquin750@gmail.com>
2026-07-15 20:52:34 +0800

    added bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
         $middleware->alias([
             'admin' => \App\Http\Middleware\AdminMiddleware::class,
